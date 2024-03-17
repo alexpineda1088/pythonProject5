96d4d9b03bfbf76dc02348439bd487d84b8dc3e4
Alex Pineda <159101357+alexpineda1088@users.noreply.github.com>
2024-03-17 18:17:57 -0500

    VARIABLES array

---
+++ Variables_array.py
@@ -0,0 +1,171 @@
+#  Declare 6 variables unidimensionales y asigne valores enteros
+enteros = [1, 2, 3, 4, 5, 6]
+
+#  Declare 6 variables unidimensionales y asigne valores flotantes
+flotantes = [1.1, 2.2, 3.3, 4.4, 5.5, 6.6]
+
+# Declare 6 variables unidimensionales y asignar valores de texto
+textos = ["hola", "que tal", "como estás", "hoy", "bien", "gracias"]
+
+#Declare 6 variables unidimensionales y asignar valores de enteros, flotantes y texto
+mixtos = [1, 2.5, "tres", 4, 5.5, "seis"]
+
+#  Declare 2 variables bidimensionales 2x3, 5x5, 3x6 y asigne valores enteros (6 variables en total)
+array_2x3_int = [[1, 9, 3],
+                 [98, 5, 45]]
+
+array_2x3_int = [[8, 99, 36],
+                 [2, 8, 48]]
+
+array_5x5_int = [[13, 62, 30, 41, 55],
+                 [16, 70, 18, 91, 10],
+                 [11, 15, 23, 14, 35],
+                 [17, 1, 19, 14, 2],
+                 [21, 82, 63, 24, 75]]
+
+array_5x5_int = [[3, 6,23, 41, 78],
+                 [76, 70, 91, 96, 10],
+                 [1, 5, 23, 14, 21],
+                 [62, 1, 19, 14, 2],
+                 [21, 28, 83, 28, 74]]
+
+array_3x6_int = [[81, 22, 13, 64, 55, 6],
+                 [73, 88, 9, 14, 11, 52],
+                 [13, 34, 19, 16, 47, 18]]
+
+array_3x6_int = [[54, 25, 93, 64, 65, 6],
+                 [37, 88, 96, 14, 11, 2],
+                 [16, 34, 93, 16, 75, 48]]
+# Declare 2 variables bidimensionales 2x3, 5x5, 3x6 y asigne valores flotantes (6 variables en total)
+
+array_2x3_float = [[1.1, 5.2, 7.3],
+                   [4.4, 9.5, 6.6]]
+
+array_2x3_float = [[2.1, 2.2, 6.3],
+                   [4.8, 5.5, 3.6]]
+
+array_5x5_float = [[9.1, 8.2, 3.3, 7.4, 7.5],
+                   [6.6, 5.7, 8.8, 4.9, 1.1],
+                   [31.11, 14.12, 11.13, 4.14, 15.15],
+                   [46.16, 17.17, 12.18, 9.19, 20.20],
+                   [21.21, 24.22, 26.23, 2.24, 27.25]]
+
+array_5x5_float = [[1.15, 2.28, 3.39, 45.4, 59.5],
+                   [68.6, 7.7, 85.8, 98.9, 10.81],
+                   [1.11, 52.12, 13.13, 18.14, 18.15],
+                   [16.16, 97.17, 17.18, 19.19, 29.20],
+                   [61.21, 28.22, 9.23, 24.2, 5.25]]
+
+array_3x6_float = [[5.1, 2.2, 83.3, 46.4, 75.5, 69.6],
+                   [7.7, 5.8, 7.9, 10.1, 81.11, 12.12],
+                   [58.13, 13.14, 15.15, 16.56, 17.24, 18.18]]
+
+array_3x6_float = [[1.12, 62.2, 33.3, 84.4, 56.5, 86.6],
+                   [74.7, 18.8, 9.9, 80.1, 11.11, 15.12],
+                   [43.13, 64.14, 15.15, 96.16, 14.17, 98.18]]
+#   Declare 2 variables bidimensionales 2x3, 5x5, 3x6 y asignar valores de texto (6 variables en total)
+array_2x3_str = [['apple', 'banana', 'orange'],
+                 ['grape', 'kiwi', 'melon']]
+
+array_2x3_str = [['manzana', 'banana', 'naranja'],
+                 ['uva', 'kiwi', 'melocoton']]
+
+array_5x5_str = [['apple', 'banana', 'orange', 'grape', 'kiwi'],
+                 ['melon', 'strawberry', 'pineapple', 'peach', 'pear'],
+                 ['watermelon', 'blueberry', 'raspberry', 'blackberry', 'plum'],
+                 ['cherry', 'lemon', 'lime', 'coconut', 'fig'],
+                 ['pomegranate', 'mango', 'papaya', 'apricot', 'nectarine']]
+
+array_5x5_str = [['manzana', 'banana', 'naranja', 'uva', 'kiwi'],
+                 ['melocoton', 'fresa', 'piña', 'pera', 'mango'],
+                 ['sandia', 'mora azul', 'cereza', 'tomate', 'pluma'],
+                 ['vaso', 'limon', 'lima', 'coco', 'reinaclaudia'],
+                 ['pomarosa', 'mango', 'papaya', 'achote', 'naranjilla']]
+
+array_3x6_str = [['red', 'green', 'blue', 'yellow', 'purple', 'orange'],
+                 ['pink', 'brown', 'black', 'white', 'gray', 'silver'],
+                 ['gold', 'cyan', 'magenta', 'violet', 'turquoise', 'indigo']]
+
+array_3x6_str = [['rojo', 'verde', 'azul', 'amarillo', 'morado', 'naranja'],
+                 ['rosado', 'cafe', 'negro', 'blanco', 'gris', 'plata'],
+                 ['oro', 'celeste', 'palo rosa', 'violeta', 'turqueza', 'azul cielo']]
+
+#   Declare 2 variables bidimensionales 2x3, 5x5, 3x6 y asignar valores de enteros, flotantes y texto (6 variables en total)
+array_2x3_mixed = [[51, 28.2, 'perro'],
+                   [48, 85.5, 'gato']]
+
+array_2x3_mixed = [[81, 98.2, 'lobo'],
+                   [78, 65.5, 'gallo']]
+
+array_5x5_mixed = [[15, 24.2, 'raton', 4, 59.5],
+                   [68, 7.87, 'gusano', 89, 19.9],
+                   [90, 91.11, 'grillo', 12, 17.13],
+                   [14, 18.15, 'tigre', 86, 16.17],
+                   [88, 79.19, 'asno', 27, 27.21]]
+
+array_5x5_mixed = [[5, 4.2, 'ganso', 4, 59.5],
+                   [88, 7.87, 'culebra', 39, 1.9],
+                   [97, 91.11, 'sapo', 2, 17.13],
+                   [84, 18.15, 'jirafa', 86, 16.7],
+                   [18, 49.19, 'aguila', 27, 67.27]]
+
+array_3x6_mixed = [['casa', 51, 25.2, 36, 'verde', 14],
+                   [5.5, 'pera', 36, 57, 8.8, 'amarilla'],
+                   [99, 'puerta', 10, 15.11, 12, 'tomate']]
+
+array_3x6_mixed = [['dormitorio', 21, 62.2, 36, 'oscuro', 46],
+                   [53.5, 'patio', 66, 7, 89.8, 'hojas'],
+                   [19, 'techo', 50, 61.11, 18, 'azul']]
+
+# Declare 2 variables tridimensionales 2x3x2, 5x5x5, 3x6x2 y asigne valores enteros (6 variables en total)
+# 2x3x2
+int_2x3x2 = [[[1, 2], [3, 4], [5, 6]],
+              [[7, 8], [9, 10], [11, 12]]]
+
+# 5x5x5
+int_5x5x5 = [[[i*5*5 + j*5 + k + 1 for k in range(5)] for j in range(5)] for i in range(5)]
+
+# 3x6x2
+int_3x6x2 = [[[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]],
+             [[13, 14], [15, 16], [17, 18], [19, 20], [21, 22], [23, 24]],
+             [[25, 26], [27, 28], [29, 30], [31, 32], [33, 34], [35, 36]]]
+
+
+# .Declare 2 variables tridimensionales 2x3x1, 5x5x2, 3x6x5 y asigne valores flotantes (6 variables en total)
+
+float_2x3x1 = [[[1.1], [2.2], [3.3]],
+               [[4.4], [5.5], [6.6]]]
+
+# 5x5x2
+float_5x5x2 = [[[i*5*2 + j*2 + k + 0.1 for k in range(2)] for j in range(5)] for i in range(5)]
+
+# 3x6x5
+float_3x6x5 = [[[i*6*5 + j*5 + k + 0.01 for k in range(5)] for j in range(6)] for i in range(3)]
+
+
+# Declare 2 variables tridimensionales 2x3x3, 5x5x4, 3x6x1 y asignar valores de texto (6 variables en total)
+# 2x3x3
+text_2x3x3 = [[['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']],
+              [['j', 'k', 'l'], ['m', 'n', 'o'], ['p', 'q', 'r']]]
+
+# 5x5x4
+text_5x5x4 = [[['t'*(i*5*4 + j*4 + k) for k in range(4)] for j in range(5)] for i in range(5)]
+
+# 3x6x1
+text_3x6x1 = [[['hello'], ['world'], ['!']],
+              [['python'], ['is'], ['awesome']],
+              [['open'], ['ai'], ['rocks']]]
+
+
+# .Declare 2 variables tridimensionales 2x3x2, 5x5x1, 3x6x2 y asignar valores de enteros, flotantes y texto (6 variables en total)
+# 2x3x2
+mixed_2x3x2 = [[[1, 2], [3.3, 4.4], ['a', 'b']],
+               [[5, 6], [7.7, 8.8], ['c', 'd']]]
+
+# 5x5x1
+mixed_5x5x1 = [[[i*5 + j + 0.1 for j in range(5)] for i in range(5)]]
+
+# 3x6x2
+mixed_3x6x2 = [[[1, 2.2], [3, 4.4], ['e', 'f']],
+               [[5, 6.6], [7, 8.8], ['g', 'h']],
+               [[9, 10.10], [11, 12.12], ['i', 'j']]]
